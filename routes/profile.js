231b86a6bd3e233de610ae243c99db27e408ef4e
vvictorious <vvictorious30@gmail.com>
2020-03-03 14:25:11 -0700

    added some error handling and the back end is looking complete for now

--- routes/profile.js
+++ routes/profile.js
@@ -14,6 +14,11 @@ router.get('/:platform/:gamertag', async (req, res) => {
             headers
         })
         const data = await response.json();
+        if (data.errors && data.errors.length > 0) {
+            return res.status(404).json({
+                message: 'Profile not found'
+            });
+        }
         res.json(data)
     } catch(err) {
         console.error(err);
