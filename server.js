434843ab68c094ccc9817468e1d4dedc61f98bb3
vvictorious <vvictorious30@gmail.com>
2020-03-03 13:16:21 -0700

    added a router file with a get route and added morgan middleware to show details upon logging

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ dotenv.config({ path: './config.env'});
 
 const app = express();
 
+//dev logging
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
+
 const port = process.env.PORT || 8000
 
 app.listen(port, () => {
