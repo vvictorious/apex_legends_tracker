ad66414f01bb8b156cb56a8dfef5c76f5cf356b1
vvictorious <vvictorious30@gmail.com>
2020-03-08 11:46:23 -0600

    rendering character name on the ui

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        profile
-    </div>
+   <section>
+       <div v-if="loading">
+           <h3>Loading...</h3>
+       </div>
+       <div v-if="error">
+           <h1>{{error}}</h1>
+           <router-link to="/">Go Back</router-link>
+       </div>
+       <div v-if="profileData" class="container">
+           {{profileData.metadata.activeLegendName}}
+       </div>
+   </section>
 </template>
 
 <script>
